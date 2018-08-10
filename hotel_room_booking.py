1dcf135e6a13339ac218468c7e5dfd1c2c7a6944
harshithaswarup <harshitha@5gindia.net>
2018-08-10 17:42:23 +0530

    final_program

--- hotel_room_booking.py
+++ hotel_room_booking.py
@@ -1,7 +1,6 @@
 import datetime
-from datetime import timedelta
 class Room:
-    def get_room_cost(self):
+    def get_cost(self):
         wkd_cost=500
         wknd_cost=600
         discount=0.05
@@ -11,23 +10,22 @@ class Room:
         cust_type=['Normal','Reward']
         return cust_type
     
-    def get_check_in_date(self):
-        print "Enter the date of check-in:"
+    def get_check_in_date(self,cust_type):
+        cust_inp=raw_input("enter the type of customer:")
         year=int(input("enter a year:"))
         month=int(input("enter a month:"))
         day=int(input("enter the day:"))
         check_in_date=datetime.date(year,month,day)
-        check_out_date=check_in_date+timedelta(2)
-        self.day_1=check_in_date.weekday()
-        return check_in_date,check_out_date
-
-    def get_no_days(self,check_in_date,check_out_date):
+        day_1=check_in_date.weekday()
+        return check_in_date,cust_inp,day_1
+    
+    def get_no_days(self,check_in_date):
         #days=check_out_date-check_in_date
         days=2
         return days
     
-    def get_total_cost(self,days,wkd_cost,wknd_cost):
-        if self.day_1 in range(0,5):
+    def get_total_cost(self,days,wkd_cost,wknd_cost,day_1):
+        if day_1 in range(0,5):
             total_cost=(days)*(wkd_cost)
             return total_cost
         else:
@@ -39,21 +37,34 @@ class Room:
             discount_amt=(total_cost)*(discount)
             return discount_amt
 
-    def get_final_cost(self,total_cost,discount_amt,cust_type):
-        cust_inp=raw_input("enter the type:")
-        if(cust_inp==cust_type[1] and total_cost>1000):
+    def get_final_cost(self,total_cost,discount_amt,cust_inp):
+        if(cust_inp=='Reward' and total_cost>1000):
             discount_cost=(total_cost)-(discount_amt)
-            print "The cost of the room after discount is:",discount_cost
-        else:
-            print "The cost of the room is:",total_cost
+            return discount_cost
 
-    
+    def display_Room_cost(self,discount_cost,total_cost,cust_inp,cust_type):
+        if(cust_inp=='Reward' and total_cost>1000):
+            print "The discount cost is:",discount_cost
+        else:
+            print "The total cost is:",total_cost
+        user_input=raw_input("would you like to know some more details?:")
+        if(user_input=='yes'):
+            check_in_date,cust_inp,day_1=r.get_check_in_date(cust_type)
+            total_cost=r.get_total_cost(days,wkd_cost,wknd_cost,day_1)
+            discount_amt=r.get_discount(total_cost,discount)
+            discount_cost=r.get_final_cost(total_cost,discount_amt,cust_inp)
+            r.display_Room_cost(discount_cost,total_cost,cust_inp,cust_type)
+        else:
+            return
+                
 
 r=Room()
-wkd_cost,wknd_cost,discount=r.get_room_cost()
+wkd_cost,wknd_cost,discount=r.get_cost()
 cust_type=r.get_cust_details()
-check_in_date,check_out_date=r.get_check_in_date()
-days=r.get_no_days(check_in_date, check_out_date)
-total_cost=r.get_total_cost(days,wkd_cost,wknd_cost)
+check_in_date,cust_inp,day_1=r.get_check_in_date(cust_type)
+days=r.get_no_days(check_in_date)
+total_cost=r.get_total_cost(days,wkd_cost,wknd_cost,day_1)
 discount_amt=r.get_discount(total_cost,discount)
-r.get_final_cost(total_cost,discount_amt,cust_type)
+discount_cost=r.get_final_cost(total_cost,discount_amt,cust_inp)
+r.display_Room_cost(discount_cost,total_cost,cust_inp,cust_type)
+
