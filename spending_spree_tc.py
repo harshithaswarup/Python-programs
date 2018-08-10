134cc058c6c5d376bd7e324dd0c86fdc5a8bfb85
harshithaswarup <harshitha@5gindia.net>
2018-08-10 17:45:20 +0530

    spending_test_case

--- spending_spree_tc.py
+++ spending_spree_tc.py
@@ -1,37 +1,48 @@
 import unittest
-from spending_spree import *
+from spending_spree_pgrm import *
 
 class TestString(unittest.TestCase):
-    def test_init_bal(self):
-        m=Money()
-        self.balance=m.get_initial_balance()
-        self.assertEqual(self.balance,1000)
+    def test_bal(self):
+        m=Money(1000)
+        m.get_initial_balance()
+        val = m.get_balance('yes',100)
+        self.assertEquals(m.balance,900)
 
-    def test_init_bal_1(self):
-        m=Money()
-        self.balance=m.get_initial_balance()
-        self.assertNotEqual(self.balance,1000)
+    def test_bal_1(self):
+        m=Money(1000)
+        m.get_initial_balance()
+        val = m.get_balance('no',100)
+        self.assertEquals(m.balance,1000)
 
-    def test_balance(self):
-        m=Money()
-        self.item_cost=m.get_item_cost()
-        self.assertNotEqual(self.item_cost,200)
+    def test_bal2(self):
+        m=Money(1000)
+        m.get_initial_balance()
+        val=m.get_balance('yes',1000)
+        self.assertEqual(m.balance,0)
 
-    def test_check1(self):
-        m=Money()
-        self.sub_balance=m.get_balance(item_cost,x,user_choice,balance)
-        self.assertNotEqual(self.sub_balance,-300)
-    
-        
-    def test_debt(self):
-        m=Money()
-        self.debt_amt=m.get_debt_amount(300,200)
-        self.assertEqual(self.debt_amt,100)
+    def test_debt_amt(self):
+        m=Money(1000)
+        m.get_initial_balance()
+        debt_amt=m.get_debt_amt(2000)
+        self.assertEqual(debt_amt,1000)
 
-    def test_debt_1(self):
-        m=Money()
-        self.debt_amt=m.get_debt_amount(500,200)
-        self.assertNotEqual(self.debt_amt,-300)
+    def test_debt_amt1(self):
+        m=Money(1000)
+        m.get_initial_balance()
+        debt_amt=m.get_debt_amt(1400)
+        self.assertEqual(debt_amt,400)
+
+    def test_debt_amt2(self):
+        m=Money(1000)
+        m.get_initial_balance()
+        debt_amt=m.get_debt_amt(-1400)
+        self.assertEqual(debt_amt,None)
+
+    def test_debt_amt3(self):
+        m=Money(1000)
+        m.get_initial_balance()
+        debt_amt=m.get_debt_amt(800)
+        self.assertNotEqual(debt_amt,200)
         
 
 if __name__=='__main__':
